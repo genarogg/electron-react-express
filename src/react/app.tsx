e877c037411f6626a6ec53a13ced8aa4bb384cae
Genaro Gonzalez <genarrogg@gmail.com>
2024-09-25 17:29:09 -0400

    ya funciona sass

--- src/react/app.tsx
+++ src/react/app.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./react/pages/Login";
-// import "./styles.scss"; // Importa tu archivo SCSS
+import Login from "./pages/Login";
+import "./scss/style.scss";
 
 const root = createRoot(document.body);
 root.render(
--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -4,10 +4,20 @@ import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
 import { rules } from "./webpack.rules";
 import { plugins } from "./webpack.plugins";
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: "style-loader" }, { loader: "css-loader" }],
-});
+rules.push(
+  {
+    test: /\.css$/,
+    use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+  },
+  {
+    test: /\.scss$/,
+    use: [
+      { loader: "style-loader" },
+      { loader: "css-loader" },
+      { loader: "sass-loader" },
+    ],
+  }
+);
 
 export const rendererConfig: Configuration = {
   module: {
