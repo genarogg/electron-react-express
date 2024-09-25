96fb3ddf3ae348dcf0e7e0d1688fea9042a1f3c3
Genaro Gonzalez <genarrogg@gmail.com>
2024-09-25 16:53:10 -0400

    ya funciona react

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,13 @@
 import { createRoot } from "react-dom/client";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./react/pages/Login.tsx";
+// import "./styles.scss"; // Importa tu archivo SCSS
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  </Router>
+);
---
+++ src/react/pages/Login.tsx
@@ -0,0 +1,7 @@
+interface LoginProps {}
+
+const Login: React.FC<LoginProps> = () => {
+  return <p>hola</p>;
+};
+
+export default Login;
